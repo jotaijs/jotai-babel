4abce84d786c01de48af1b4b7c2521e52576e7ab
Wendy Fouchter <wendy.fouchter@gmail.com>
2026-05-26 12:16:42 +0200

    add jotai-tanstack-query, jotai-eager and jotai-effect atoms (#1)
    * add jotai-tanstack-query and jotai-eager atoms
    
    * add jotai-effect atom
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -61,4 +61,16 @@ const atomFunctionNames = [
   'atomWithCache',
   // jotai-recoil
   'atomWithRecoilValue',
+  // jotai-tanstack-query
+  'atomWithQuery',
+  'atomWithQueries',
+  'atomWithInfiniteQuery',
+  'atomWithMutation',
+  'atomWithSuspenseQuery',
+  'atomWithSuspenseInfiniteQuery',
+  'atomWithMutationState',
+  // jotai-eager
+  'eagerAtom',
+  // jotai-effect
+  'atomEffect',
 ];
